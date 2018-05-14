9883a2c8e50cedb1afbe96a17b6eb74d11f0ab2b
mark <ragzouken@gmail.com>
2018-05-14 08:59:29 +0100

    async load bitsy file

--- src/PixiTest.tsx
+++ src/PixiTest.tsx
@@ -3,7 +3,21 @@ import * as Pixi from 'pixi.js';
 
 import { App } from "./App";
 import { renderTileset } from "./Rendering";
-import { BitsyParser } from "@bitsy/parser";
+import { BitsyParser, BitsyWorld } from "@bitsy/parser";
+
+function loadText(url: string, callback: (text: string) => void): void
+{
+  const rawFile = new XMLHttpRequest();
+  rawFile.open("GET", url, false);
+	rawFile.onreadystatechange = () => {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        callback(rawFile.responseText);
+      }
+    }
+  };
+  rawFile.send(null);
+}
 
 interface IMainProps {
   app : App
@@ -13,25 +27,37 @@ interface IMainState {
 
 }
 
-const bitsy = document.getElementById("gamedata")!.innerText;
-const world = BitsyParser.parse(bitsy.split("\n"));
-const tileset = renderTileset(world);
-
 export class PixiComponent extends React.Component<IMainProps, IMainState> {
   private app: Pixi.Application;
   private gameCanvas: HTMLDivElement;
 
   private sprite: Pixi.Sprite;
+  private world: BitsyWorld;
+  private tileset: Pixi.Texture[];
 
   constructor(props : IMainProps) {
     super(props);
   }
   
+  private loadTexture(): void
+  {
+    loadText("./955B75C9.bitsy.txt", text =>
+    {
+      this.world = BitsyParser.parse(text.split("\r\n"));
+      this.tileset = renderTileset(this.world);
+
+      const test = new PIXI.Texture(this.tileset[0].baseTexture)
+      this.sprite = new PIXI.Sprite(test);
+      this.sprite.pivot = new Pixi.Point(128, 128);
+      this.app.stage.addChild(this.sprite);
+    });
+  }
+
   public refresh()
   {
-    const keys = Object.keys(world.tiles);
+    const keys = Object.keys(this.world.tiles);
     const index = Math.floor(Math.random() * keys.length);
-    this.sprite.texture = tileset[index];
+    this.sprite.texture = this.tileset[index];
   }
 
   /**
@@ -42,13 +68,9 @@ export class PixiComponent extends React.Component<IMainProps, IMainState> {
     this.gameCanvas.appendChild(this.app.view);
     this.app.start();
 
-    const test = new PIXI.Texture(tileset[0].baseTexture);
-
-    this.sprite = new PIXI.Sprite(test);
-    this.sprite.pivot = new Pixi.Point(128, 128);
-    this.app.stage.addChild(this.sprite);
-
     //this.refresh();
+
+    this.loadTexture();
 
     let orig: Pixi.Point | null;
     let drag: Pixi.Point | null;
