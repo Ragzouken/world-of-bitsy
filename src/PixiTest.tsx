745cee126691472a8f943052f5a26c38ba20409c
mark <ragzouken@gmail.com>
2018-05-18 15:02:15 +0100

    fix a load of things, ctrl+click to open the game

--- src/PixiTest.tsx
+++ src/PixiTest.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as Pixi from 'pixi.js';
 
 import { App } from "./App";
-import { queueTileset, MTexture, renderQueuedTile, queue, coord2boid } from "./Rendering";
+import { queueGraphics, MTexture, renderQueuedTile, queue, coord2boid } from "./Rendering";
 import { BitsyParser, BitsyWorld } from "@bitsy/parser";
 
 const parsecsv: (csv: string) => string[][] = require("csv-parse/lib/sync");
@@ -10,16 +10,16 @@ let offset: number = 0;
 
 let textureIndex = 0;
 const textures = [
-  new MTexture(  8,   8),
-  new MTexture( 16,  16),
-  new MTexture( 32,  32),
-  new MTexture( 64,  64),
-  new MTexture(128, 128),
-  new MTexture(256, 256),
-  new MTexture(512, 512)
+  [new MTexture(  8,   8), new MTexture(  8,   8)],
+  [new MTexture( 16,  16), new MTexture( 16,  16)],
+  [new MTexture( 32,  32), new MTexture( 32,  32)],
+  [new MTexture( 64,  64), new MTexture( 64,  64)],
+  [new MTexture(128, 128), new MTexture(128, 128)],
+  [new MTexture(256, 256), new MTexture(256, 256)],
+  [new MTexture(512, 512), new MTexture(512, 512)]
 ];
 
-let mtex = textures[textureIndex];// new MTexture(512, 512);
+const sprites: Pixi.extras.TilingSprite[][] = [];
 
 const sizes = [
   1,
@@ -41,19 +41,35 @@ const offsets = [
   -1
 ];
 
-function elevate(): MTexture
+function makeSprite(texture: MTexture): PIXI.extras.TilingSprite
+{
+  const test = new PIXI.Texture(texture.base);
+  const sprite = new PIXI.extras.TilingSprite(test, 512, 512);
+  sprite.scale = new Pixi.Point(2, 2);
+  sprite.visible = false;
+
+  return sprite;
+}
+
+function elevate(): MTexture[]
 {
   const prev = textures[textureIndex];
   textureIndex += 1;
   const next = textures[textureIndex];
 
-  const w = prev.canvas.width;
-  const h = prev.canvas.height;
+  const w = prev[0].canvas.width;
+  const h = prev[0].canvas.height;
 
-  next.context.drawImage(prev.canvas, 0, 0);
-  next.context.drawImage(prev.canvas, w, 0);
-  next.context.drawImage(prev.canvas, 0, h);
-  next.context.drawImage(prev.canvas, w, h);
+  for (let i = 0; i < 2; ++i)
+  {
+    const next_ = next[i];
+    const prev_ = prev[i];
+
+    next_.context.drawImage(prev_.canvas, 0, 0);
+    next_.context.drawImage(prev_.canvas, w, 0);
+    next_.context.drawImage(prev_.canvas, 0, h);
+    next_.context.drawImage(prev_.canvas, w, h);
+  }
 
   return next;
 }
@@ -80,9 +96,9 @@ interface IMainState {
 
 }
 
-function norm(coord: number): number
+function norm(coord: number, offset: number = 0): number
 {
-  const n = sizes[textureIndex];
+  const n = sizes[textureIndex + offset];
 
   return ((coord % n) + n) % n;
 }
@@ -91,7 +107,6 @@ export class PixiComponent extends React.Component<IMainProps, IMainState> {
   private app: Pixi.Application;
   private gameCanvas: HTMLDivElement;
 
-  private sprite: PIXI.extras.TilingSprite;
   private world: BitsyWorld;
 
   private index?: string[][];
@@ -101,16 +116,14 @@ export class PixiComponent extends React.Component<IMainProps, IMainState> {
     super(props);
   }
   
-  private addTileset(boid: string, log: string): void
+  private addTileset(boid: string, entry: string[]): void
   {
     const url = `https://raw.githubusercontent.com/Ragzouken/bitsy-archive/master/${boid}.bitsy.txt`
 
     loadText(url, text =>
     {
       this.world = BitsyParser.parse(text.split("\n"));
-      queueTileset(this.world, log);
-      //console.log(log);
-      log = log + boid;
+      queueGraphics(this.world, entry);
     });
   }
 
@@ -127,7 +140,7 @@ export class PixiComponent extends React.Component<IMainProps, IMainState> {
 
     if (entry[3].trim() in this.missing) return false;
 
-    this.addTileset(boid, `${entry[2]} by ${entry[3]}`);
+    this.addTileset(boid, entry);
 
     return true;
   }
@@ -172,26 +185,78 @@ export class PixiComponent extends React.Component<IMainProps, IMainState> {
 
     this.app.stage.interactive = true;
 
+    function getGame(x: number, y: number): string[]
+    {
+      x = norm(x);
+      y = norm(y);
+
+      let entry = coord2boid[`${x},${y}`];
+
+      if (!entry)
+      {
+        x = norm(x, -1);
+        y = norm(y, -1);
+        return coord2boid[`${x},${y}`];
+      }
+
+      return entry;
+    }
+
     this.app.stage.on("pointerdown", (event: Pixi.interaction.InteractionEvent) => {
       drag = event.data.getLocalPosition(this.app.stage);
-      orig = new Pixi.Point(this.sprite.tilePosition.x, this.sprite.tilePosition.y);
+      orig = new Pixi.Point(tilePosX, tilePosY);
 
-      const coord = event.data.getLocalPosition(this.sprite);
+      const coord = event.data.getLocalPosition(sprites[0][0]);
 
-      coord.x -= this.sprite.tilePosition.x;
-      coord.y -= this.sprite.tilePosition.y;
+      coord.x -= tilePosX;
+      coord.y -= tilePosY;
 
       const x = norm(Math.floor(coord.x / 8));
       const y = norm(Math.floor(coord.y / 8));
 
-      console.log(coord2boid[`${x},${y}`]);
+      const entry = getGame(x, y);
+
+      if (entry)
+      {
+        console.log(`${entry[2]} by ${entry[3]}`);
+      }
+      else
+      {
+        console.log(`${x},${y} of ${textureIndex}`);
+      }
     });
+
+    document.onmousedown = (event: MouseEvent) =>
+    {
+      if (event.ctrlKey || event.metaKey)
+      {
+        let coordX = event.clientX;
+        let coordY = event.clientY; 
+
+        coordX -= tilePosX;
+        coordY -= tilePosY;
+
+        const x = norm(Math.floor(coordX / 8));
+        const y = norm(Math.floor(coordY / 8));
+
+        const entry = getGame(x, y);
+
+        if (entry)
+        {
+          const url = entry[4];
+          window.open(url,'_blank');
+        }
+      }
+    };
 
     const resetDrag = () => 
     {
       drag = null;
       orig = null;
     };
+
+    let tilePosX = 0;
+    let tilePosY = 0;
 
     this.app.stage.on("pointerup", (event: Pixi.interaction.InteractionEvent) => resetDrag());
     document.onpointerup = () => resetDrag();
@@ -204,17 +269,19 @@ export class PixiComponent extends React.Component<IMainProps, IMainState> {
       const dx = m.x - drag.x;
       const dy = m.y - drag.y;
 
-      this.sprite.tilePosition.x = orig.x + (dx / 2);
-      this.sprite.tilePosition.y = orig.y + (dy / 2);
+      tilePosX = orig.x + (dx / 2);
+      tilePosY = orig.y + (dy / 2);
     });
 
-    const test = new PIXI.Texture(mtex.base);
-    this.sprite = new PIXI.extras.TilingSprite(test, this.app.renderer.width, this.app.renderer.height); //new PIXI.Sprite(test);
-    //this.sprite.pivot = new Pixi.Point(256, 256);
-    //this.sprite.x = 256;
-    //this.sprite.y = 256;
-    this.sprite.scale = new Pixi.Point(2, 2);
-    this.app.stage.addChild(this.sprite);
+    for (let i = 0; i < textures.length; ++i)
+    {
+      const f1 = makeSprite(textures[i][0]);
+      const f2 = makeSprite(textures[i][1]);
+
+      sprites.push([f1, f2])
+      this.app.stage.addChild(f1);
+      this.app.stage.addChild(f2);
+    }
 
     document.body.style.overflow = 'hidden';
 
@@ -227,8 +294,6 @@ export class PixiComponent extends React.Component<IMainProps, IMainState> {
       this.app.renderer.view.style.height = h + "px";    
       //this part adjusts the ratio:    
       this.app.renderer.resize(w,h);
-      this.sprite.width = this.app.renderer.width;
-      this.sprite.height = this.app.renderer.height;
     };
     
     window.onresize = (event) =>
@@ -238,13 +303,37 @@ export class PixiComponent extends React.Component<IMainProps, IMainState> {
 
     let timer = 0;
     let timer2 = 0;
+    let timer3 = 0;
+
+    let frame = 0;
 
     this.app.ticker.add(delta => 
     {
       //this.sprite.scale = new Pixi.Point(2, 2);
 
+      for (let pair of sprites)
+      {
+        for (let frame of pair)
+        {
+          frame.tilePosition.x = tilePosX;
+          frame.tilePosition.y = tilePosY;
+          frame.width = this.app.renderer.width;
+          frame.height = this.app.renderer.height;
+          frame.visible = false;
+        }
+      }
+
+      sprites[textureIndex][frame].visible = true;
+
       timer += delta;
       timer2 += delta;
+      timer3 += (delta / 60);
+
+      if (timer3 >= .4)
+      {
+        timer3 -= .4;
+        frame = 1 - frame;
+      }
 
       if (timer >= 15 && queue.length < 64)
       {
@@ -260,19 +349,12 @@ export class PixiComponent extends React.Component<IMainProps, IMainState> {
 
         if (offset == offsets[textureIndex])
         {
-          mtex = elevate();
-          
-          this.app.stage.removeChild(this.sprite);
-
-          const prev = this.sprite;
-          const test = new PIXI.Texture(mtex.base);
-          this.sprite = new PIXI.extras.TilingSprite(test, this.app.renderer.width, this.app.renderer.height);
-          this.sprite.scale = new Pixi.Point(2, 2);
-          this.sprite.tilePosition = prev.tilePosition;
-          this.app.stage.addChild(this.sprite);
+          elevate();
         }
 
-        offset = renderQueuedTile(mtex, offset);
+        offset = renderQueuedTile(textures[textureIndex][0],
+                                  textures[textureIndex][1], 
+                                  offset);
       }
 
       resize();
